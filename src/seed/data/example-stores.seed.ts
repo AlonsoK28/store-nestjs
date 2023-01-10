ff894a827f941006ad77f14a99ca88e4cd4026b3
AlonsoK28 <calonso011@yahoo.com.mx>
2023-01-09 21:44:37 -0600

    updated example data

--- src/seed/data/example-stores.seed.ts
+++ src/seed/data/example-stores.seed.ts
@@ -3,30 +3,26 @@ import { Store } from 'src/store/entities/store.entity';
 export const ExampleStoresSeed: Store[] = [
   {
     id: 1,
-    // category: 'Clothes',
+    category: 'Clothes',
     name: 'adidas clothes méxico',
-    // country: 'México',
+    country: 'México',
     createdAt: Date.now(),
   },
   {
     id: 2,
-    // category: 'Clothes',
+    category: 'Clothes',
     name: 'sterilite méxico',
-    // country: 'México',
+    country: 'México',
     createdAt: Date.now(),
   },
   {
     id: 3,
-    // category: 'Clothes',
     name: 'amasón méxico',
-    // country: 'México',
     createdAt: Date.now(),
   },
   {
     id: 4,
-    // category: 'Clothes',
     name: 'apple méxico',
-    // country: 'México',
     createdAt: Date.now(),
   },
 ];
