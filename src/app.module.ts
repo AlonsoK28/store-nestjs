8d30ac4810b8415b259fc0d7e639decea6ae4ec2
C Alonso C Ortega <calonso011@gmail.com>
2024-02-11 22:56:32 -0600

    added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
+
+// controllers
 import { AppController } from './app.controller';
+
+// services
 import { AppService } from './app.service';
-import { StoreModule } from './store/store.module';
 import { IdGeneratorService } from './services/id-generator/id-generator.service';
+
+// modules
+import { StoreModule } from './store/store.module';
 import { SeedModule } from './seed/seed.module';
 
+// config
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [StoreModule, SeedModule],
-  controllers: [AppController],
-  providers: [AppService, IdGeneratorService],
+  imports: [
+    // config based on docs https://docs.nestjs.com/techniques/configuration
+    ConfigModule.forRoot(),
+    StoreModule, 
+    SeedModule
+  ],
+  controllers: [
+    AppController
+  ],
+  providers: [
+    AppService, 
+    IdGeneratorService
+  ],
 })
 export class AppModule {}
